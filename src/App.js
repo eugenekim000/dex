253c949a5a3999d16f5a8112131aab94031cfd6d
eugenekim000 <eugenekim000@gmail.com>
2020-10-07 22:54:15 -0700

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import CatchList from "./components/CatchList";
+import Header from "./components/Header";
 import HomeGrid from "./components/HomeGrid";
 
 function App() {
   return (
     <div className="App">
-      <HomeGrid />
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomeGrid} />
+        <Route path="/catch-list" component={CatchList} />
+      </Switch>
     </div>
   );
 }
